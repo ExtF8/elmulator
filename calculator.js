4af6d5cf6238d0860044fd6863724cc365fa8f34
extf8 <extfield8@gmail.com>
2023-07-29 14:57:56 +0200

    Refactored calculations?Added updateValues func

--- calculator.js
+++ calculator.js
@@ -18,14 +18,17 @@ getValue1 = () => {
 
 const calculations = (input) => {
     const output2 = +(input * RESISTANCE_ONE).toFixed(2);
-    outputTwoValue.value = output2;
-
     const output3 = +(input * RESISTANCE_TWO).toFixed(2);
-    outputThreeValue.value = output3;
-
     const output4 = +(input + output2 / 4).toFixed(2);
-    outputFourValue.value = output4;
-
     const output5 = +(input + output3 / 4).toFixed(2);
-    outputFiveValue.value = output5;
+    const outputs = [output2, output3, output4, output5];
+
+    updateValues(outputs);
+};
+
+const updateValues = (outputs) => {
+    outputTwoValue.value = outputs[0];
+    outputThreeValue.value = outputs[1];
+    outputFourValue.value = outputs[2];
+    outputFiveValue.value = outputs[3];
 };
