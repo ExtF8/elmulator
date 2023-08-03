752eec6e308987d2073cc945d2ee2894d957689f
extf8 <extfield8@gmail.com>
2023-08-03 15:45:04 +0300

    Refactored

--- calculator.js
+++ calculator.js
@@ -1,34 +1,41 @@
-let inputNumberValue = document.getElementById('input1');
-let outputTwoValue = document.getElementById('input2');
-let outputThreeValue = document.getElementById('input3');
-let outputFourValue = document.getElementById('input4');
-let outputFiveValue = document.getElementById('input5');
+let inputElement = document.getElementById('input1');
+
+let outputTwoElement = document.getElementById('input2');
+let outputThreeElement = document.getElementById('input3');
+let outputFourElement = document.getElementById('input4');
+let outputFiveElement = document.getElementById('input5');
 
 const RESISTANCE_ONE = 1.67;
 const RESISTANCE_TWO = 2.67;
 
-inputNumberValue.addEventListener('input', (e) => {
-    let input = getValue1();
-    calculations(input);
+inputElement.addEventListener('input', () => {
+    calculations(getInputValue());
 });
 
-getValue1 = () => {
-    return Number(inputNumberValue.value);
+const getInputValue = () => {
+    return Number(inputElement.value);
 };
 
-const calculations = (input) => {
-    const output2 = Number(+(input * RESISTANCE_ONE)).toFixed(2);
-    const output3 = Number(+(input * RESISTANCE_TWO)).toFixed(2);
-    const output4 = Number(+((input + Number(output2)) / 4)).toFixed(2);
-    const output5 = Number(+((input + Number(output3)) / 4)).toFixed(2);
-    const outputs = [output2, output3, output4, output5];
+const calculations = (inputValue) => {
+    const outputTwo = (inputValue * RESISTANCE_ONE).toFixed(2);
+    const outputThree = (inputValue * RESISTANCE_TWO).toFixed(2);
+    const outputFour = (inputValue + Number(outputTwo) / 4).toFixed(2);
+    const outputFive = (inputValue + Number(outputThree) / 4).toFixed(2);
 
-    updateValues(outputs);
+    const outputs = [outputTwo, outputThree, outputFour, outputFive];
+
+    updateOutputValues(outputs);
 };
 
-const updateValues = (outputs) => {
-    outputTwoValue.value = outputs[0];
-    outputThreeValue.value = outputs[1];
-    outputFourValue.value = outputs[2];
-    outputFiveValue.value = outputs[3];
+const updateOutputValues = (outputs) => {
+    const outputElements = [
+        outputTwoElement,
+        outputThreeElement,
+        outputFourElement,
+        outputFiveElement,
+    ];
+
+    for (let i = 0; i < outputElements.length; i++) {
+        outputElements[i].value = outputs[i];
+    }
 };
