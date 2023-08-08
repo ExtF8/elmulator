7260283d23a3ef5d339d1d046de2b7606b4f544d
extf8 <extfield8@gmail.com>
2023-08-08 17:25:55 +0300

    Added style for active page

--- renderer.js
+++ renderer.js
@@ -1,33 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('[data-load-page]');
-    // const container
+    const buttons = document.querySelectorAll('[data-load-page]', 'button');
     const container = document.getElementById('pageContainer');
 
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
             const pageToLoad = e.target.getAttribute('data-load-page');
-            // loadPage(pageToLoad);
+
+            buttons.forEach((button) => button.classList.remove('active'));
 
             fetch(`${pageToLoad}`)
                 .then((response) => response.text())
                 .then((html) => {
                     container.innerHTML = html;
                 });
+            e.target.classList.add('active');
         });
     });
 });
-
-// function loadPage(page) {
-//     console.log(`Attempting to load: ${page}`);
-//     if (!container) {
-//         console.error('Container not found!');
-//         return;
-//     }
-
-    // Using htmx to load content
-    // container.setAttribute('hx-get', `${page}`);
-    // container.setAttribute('hx-trigger', 'load');
-
-    // Preform action above
-    // htmx.trigger('load');
-// }
