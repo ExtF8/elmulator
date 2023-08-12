8bb1a4d544bbdab96a70bccd101b322de1bdae26
extf8 <extfield8@gmail.com>
2023-08-12 19:04:58 +0300

    Added loadScript func

--- renderer.js
+++ renderer.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-load-page]', 'button');
     const container = document.getElementById('pages_container');
 
+    // Script mapping
+    const scriptMapping = {
+        './pages/circuitsCount.html': './scriptsPages/circuitsCalculator.js',
+        './pages/externalImpedance.html': './scriptsPages/impedanceCalculator.js',
+        './pages/rings.html': './scriptsPages/resistanceCalculator.js',
+    };
+
     buttons.forEach((button) => {
         button.addEventListener('click', handleButtonClick);
     });
@@ -14,20 +21,38 @@ document.addEventListener('DOMContentLoaded', () => {
         activateButton(e.target);
     }
 
+    function activateButton(button) {
+        button.classList.add('active');
+    }
+
     function deactivateButtons(buttons) {
         buttons.forEach((button) => button.classList.remove('active'));
     }
-
+    // Load page
     function loadPage(url, container) {
         fetch(url)
             .then((response) => response.text())
             .then((html) => {
                 container.innerHTML = html;
             });
+            loadScript(scriptMapping[url])
     }
 
-    function activateButton(button) {
-        button.classList.add('active');
+    // Load script
+    function loadScript(scriptUrl) {
+        // Remove old script
+        const oldScript = document.getElementById('calculations_script');
+        if (oldScript) {
+            oldScript.remove();
+        }
+
+        // Create and append the new script element
+        const script = document.createElement('script');
+        script.id = 'calculations_script';
+        script.src = scriptUrl;
+        script.defer = true;
+
+        document.body.appendChild(script);
     }
 
     const defaultPage = './pages/rings.html';
