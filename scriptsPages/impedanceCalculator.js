8f0ec1cbf52d31a88ff2710867e208b0a2aab004
extf8 <extfield8@gmail.com>
2023-08-12 21:17:36 +0300

    Added Immediately Invoked Function Expression

--- scriptsPages/impedanceCalculator.js
+++ scriptsPages/impedanceCalculator.js
@@ -1,37 +1,37 @@
 // External Impedance Calculations
+(function () {
+    // Initialize values
+    let onePhaseIpfOutput = 0;
+    let threePhaseIpfOutput = 0;
 
-// Initialize values
-let onePhaseIpfOutput = 0;
-let threePhaseIpfOutput = 0;
+    let ipf = onePhaseIpfOutput;
+    let tpIpf = threePhaseIpfOutput;
 
-let ipf = onePhaseIpfOutput;
-let tpIpf = threePhaseIpfOutput;
+    // Get inputs Voltage and Input Zbd
+    const getImpedanceInputs = (event) => {
+        if (event.target.id === 'voltage_input') {
+            voltageInput = getVoltageInputValue(event.target);
+            console.log(voltageInput);
+        } else if (event.target.id === 'zbd_input') {
+            zbdInput = getZbdInputValue(event.target);
+            console.log(zbdInput);
+        }
+    };
+    document.body.addEventListener('input', getImpedanceInputs);
 
-// Get inputs Voltage and Input Zbd
-const getImpedanceInputs = (event) => {
-    if (event.target.id === 'voltage_input') {
-        voltageInput = getVoltageInputValue(event.target);
-        console.log(voltageInput);
-    } else if (event.target.id === 'zbd_input') {
-        zbdInput = getZbdInputValue(event.target);
-        console.log(zbdInput);
-    }
-};
-document.body.addEventListener('input', getImpedanceInputs);
+    // Get Voltage inputs values
+    const getVoltageInputValue = (element) => {
+        return Number(element.value);
+    };
 
-// Get Voltage inputs values
-const getVoltageInputValue = (element) => {
-    return Number(element.value);
-};
+    // Get Zbd inputs values
+    const getZbdInputValue = (element) => {
+        return Number(element.value);
+    };
 
-// Get Zbd inputs values
-const getZbdInputValue = (element) => {
-    return Number(element.value);
-};
+    // Calculate L1 Ipf output
 
+    // Calculate TP Ipf output
 
-// Calculate L1 Ipf output
-
-// Calculate TP Ipf output
-
-// Update outputs
+    // Update outputs
+});
