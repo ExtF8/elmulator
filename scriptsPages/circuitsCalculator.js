0cbff56eb4b2f873f056017f929f771fa962c484
extf8 <extfield8@gmail.com>
2023-08-12 19:52:22 +0300

    Added Immediately Invoked Function Expression

--- scriptsPages/circuitsCalculator.js
+++ scriptsPages/circuitsCalculator.js
@@ -1,79 +1,80 @@
 // Circuits Count Calculations
+(function () {
+    // Initialize values
+    let circuitsInput = 0;
+    let peopleInput = 0;
 
-// Initialize values
-let circuitsInput = 0;
-let peopleInput = 0;
+    // Get Circuits and People inputs
+    const handleCircuitsInputChanges = (event) => {
+        if (event.target.id === 'circuits_input') {
+            updateCircuitsInput(event.target);
+        } else if (event.target.id === 'people_input') {
+            updatePeopleInput(event.target);
+        }
+        calculateOutputValues();
+    };
+    document.body.addEventListener('input', handleCircuitsInputChanges);
 
-// Get Circuits and People inputs
-const handleCircuitsInputChanges = (event) => {
-    if (event.target.id === 'circuits_input') {
-        updateCircuitsInput(event.target);
-    } else if (event.target.id === 'people_input') {
-        updatePeopleInput(event.target);
-    }
-    calculateOutputValues();
-};
-document.body.addEventListener('input', handleCircuitsInputChanges);
+    // Update inputs
+    const updateCircuitsInput = (element) => {
+        circuitsInput = getCircuitsInputValue(element);
+    };
 
-// Update inputs
-const updateCircuitsInput = (element) => {
-    circuitsInput = getCircuitsInputValue(element);
-};
+    const updatePeopleInput = (element) => {
+        peopleInput = getPeopleInputValue(element);
+    };
 
-const updatePeopleInput = (element) => {
-    peopleInput = getPeopleInputValue(element);
-};
+    // Get Circuits value
+    const getCircuitsInputValue = (element) => {
+        return Number(element.value);
+    };
+    // Get People value
+    const getPeopleInputValue = (element) => {
+        return Number(element.value);
+    };
 
-// Get Circuits value
-const getCircuitsInputValue = (element) => {
-    return Number(element.value);
-};
-// Get People value
-const getPeopleInputValue = (element) => {
-    return Number(element.value);
-};
+    // Values for output
+    const calculateOutputValues = () => {
+        let circuitsPerPerson = calculateCircuitsPerPerson();
+        let remainder = calculateRemainder();
 
-// Values for output
-const calculateOutputValues = () => {
-    let circuitsPerPerson = calculateCircuitsPerPerson();
-    let remainder = calculateRemainder();
+        // For one person
+        let forOnePerson = circuitsPerPerson + remainder;
+        // For others
+        let forOthers = circuitsPerPerson;
 
-    // For one person
-    let forOnePerson = circuitsPerPerson + remainder;
-    // For others
-    let forOthers = circuitsPerPerson;
+        outputs = [forOnePerson, forOthers];
 
-    outputs = [forOnePerson, forOthers];
+        updateOutputs(outputs);
+    };
 
-    updateOutputs(outputs);
-};
+    // Per person
+    const calculateCircuitsPerPerson = () => {
+        if (!inputsAreValid()) return 0;
+        return Math.floor(circuitsInput / peopleInput);
+    };
 
-// Per person
-const calculateCircuitsPerPerson = () => {
-    if (!inputsAreValid()) return 0;
-    return Math.floor(circuitsInput / peopleInput);
-};
+    // remainder
+    const calculateRemainder = () => {
+        if (!inputsAreValid()) return 0;
+        return circuitsInput % peopleInput;
+    };
 
-// remainder
-const calculateRemainder = () => {
-    if (!inputsAreValid()) return 0;
-    return circuitsInput % peopleInput;
-};
+    // Are inputs Valid
+    const inputsAreValid = () => {
+        return circuitsInput !== 0 && peopleInput !== 0;
+    };
 
-// Are inputs Valid
-const inputsAreValid = () => {
-    return circuitsInput !== 0 && peopleInput !== 0;
-};
+    // Update calculated values
+    const updateOutputs = (outputs) => {
+        const outputElementIds = ['one_person_output', 'people_output'];
 
-// Update calculated values
-const updateOutputs = (outputs) => {
-    const outputElementIds = ['one_person_output', 'people_output'];
+        const outputElements = outputElementIds.map((id) =>
+            document.getElementById(id)
+        );
 
-    const outputElements = outputElementIds.map((id) =>
-        document.getElementById(id)
-    );
-
-    for (let i = 0; i < outputElements.length; i++) {
-        outputElements[i].value = outputs[i];
-    }
-};
+        for (let i = 0; i < outputElements.length; i++) {
+            outputElements[i].value = outputs[i];
+        }
+    };
+})();
--- scriptsPages/resistanceCalculator.js
+++ scriptsPages/resistanceCalculator.js
@@ -1,41 +1,42 @@
 // Rings resistance calculations
+(function () {
+    const RESISTANCE_ONE = 1.67;
+    const RESISTANCE_TWO = 2.67;
 
-const RESISTANCE_ONE = 1.67;
-const RESISTANCE_TWO = 2.67;
+    document.body.addEventListener('input', (event) => {
+        if (event.target.id === 'rings_input') {
+            calculations(getInputValue(event.target));
+        }
+    });
 
-document.body.addEventListener('input', (event) => {
-    if (event.target.id === 'rings_input') {
-        calculations(getInputValue(event.target));
-    }
-});
+    const getInputValue = (element) => {
+        return Number(element.value);
+    };
 
-const getInputValue = (element) => {
-    return Number(element.value);
-};
+    const calculations = (inputValue) => {
+        let outputOne = (inputValue * RESISTANCE_ONE).toFixed(2);
+        let outputTwo = (inputValue * RESISTANCE_TWO).toFixed(2);
+        let outputThree = ((inputValue + Number(outputOne)) / 4).toFixed(2);
+        let outputFour = ((inputValue + Number(outputTwo)) / 4).toFixed(2);
 
-const calculations = (inputValue) => {
-    const outputOne = (inputValue * RESISTANCE_ONE).toFixed(2);
-    const outputTwo = (inputValue * RESISTANCE_TWO).toFixed(2);
-    const outputThree = ((inputValue + Number(outputOne)) / 4).toFixed(2);
-    const outputFour = ((inputValue + Number(outputTwo)) / 4).toFixed(2);
+        const outputs = [outputOne, outputTwo, outputThree, outputFour];
 
-    const outputs = [outputOne, outputTwo, outputThree, outputFour];
+        updateOutputValues(outputs);
+    };
 
-    updateOutputValues(outputs);
-};
+    const updateOutputValues = (outputs) => {
+        const outputElementsIds = [
+            'rings_output1',
+            'rings_output2',
+            'rings_output3',
+            'rings_output4',
+        ];
+        const outputElements = outputElementsIds.map((id) =>
+            document.getElementById(id)
+        );
 
-const updateOutputValues = (outputs) => {
-    const outputElementsIds = [
-        'rings_output1',
-        'rings_output2',
-        'rings_output3',
-        'rings_output4',
-    ];
-    const outputElements = outputElementsIds.map((id) =>
-        document.getElementById(id)
-    );
-
-    for (let i = 0; i < outputElements.length; i++) {
-        outputElements[i].value = outputs[i];
-    }
-};
+        for (let i = 0; i < outputElements.length; i++) {
+            outputElements[i].value = outputs[i];
+        }
+    };
+})();
