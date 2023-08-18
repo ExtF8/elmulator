c1833c2763b8309b1a9b34215183117195603412
extf8 <extfield8@gmail.com>
2023-08-18 11:24:44 +0300

    Refactored paths

--- package-elmulator.js
+++ package-elmulator.js
@@ -1,26 +1,33 @@
+const path = require('path');
 const electronPackager = require('electron-packager');
 const setLanguages = require('./node_modules/electron-packager-languages');
 
-const PROJECT_PATH="../../../../repos/freelance/Projects/elmulator"
-const BUILD_PATH="../../Builds/builds_elmulator/"
-const ICON_PATH="./images/icons"
-const version = require(`${PROJECT_PATH}/package.json`).version;
+// Project paths
+const PROJECT_ROOT = path.resolve(__dirname, '../elmulator');
+const BUILD_PATH = path.resolve(__dirname, '../../Builds/builds_elmulator');
+const ICON_PATH = path.join(__dirname, './images/icons');
+const PACKAGE_JSON_PATH = path.join(PROJECT_ROOT, 'package.json');
+
+const { version } = require(PACKAGE_JSON_PATH);
 
 electronPackager({
-    dir: PROJECT_PATH,
+    dir: PROJECT_ROOT,
     name: `elmulator-v${version}`,
     platform: 'win32',
     arch: 'x64',
-    icon: `${ICON_PATH}/icon.ico`,
-    portable: true,
-    asar: true,
-    ignore: [".gitignore", ".vscode", "out"],
-    overwrite: true,
+    icon: path.join(ICON_PATH, 'icon.ico'),
     prune: true,
-    out: `${BUILD_PATH}`,
-    afterCopy: [setLanguages(['en', 'en_GB'])]  // Only supports the specified languages
-}).then(() => {
-    console.log('Packaging was successful');
-}).catch(error => {
-    console.error('Packaging was NOT successful', error);
-});
+    asar: true,
+    ignore: ['.gitignore', '.vscode'],
+    portable: true,
+    overwrite: true,
+    afterCopy: [setLanguages(['en', 'en_GB'])], // Only supports the specified languages
+    out: BUILD_PATH,
+    quiet: false,
+})
+    .then(() => {
+        console.log('Packaging was successful');
+    })
+    .catch((error) => {
+        console.error('Packaging was NOT successful', error);
+    });
