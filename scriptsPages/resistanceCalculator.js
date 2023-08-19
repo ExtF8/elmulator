6f4cea15a4f2e951c1d55acbc7fd2eb8473147de
extf8 <extfield8@gmail.com>
2023-08-19 12:14:26 +0300

    Refactored code and comments for clarity

--- scriptsPages/resistanceCalculator.js
+++ scriptsPages/resistanceCalculator.js
@@ -3,25 +3,30 @@
  */
 (function () {
     // Constants
-    const RESISTANCE_ONE_MULTIPLIER = 1.67;
-    const RESISTANCE_TWO_MULTIPLIER = 2.67;
+    const DEFAULT_R2_MULTIPLIER_ONE = 1.67;
+    const DEFAULT_R2_MULTIPLIER_TWO = 2.67;
+    const DIVISION_FACTOR = 4;
+
+    // The structure of the outputs object
+    const outputs = {
+        r2_multiplier_1: 0,
+        r2_multiplier_2: 0,
+        r_sum_1: 0,
+        r_sum_2: 0,
+    };
 
     /**
-     * Handles changes to the resistance input, calculates outputs, and updates them
+     * Handles changes to the resistance input and initiates the processing of resistance calculations
      * @param {Event} event - The input event
      */
     const handleInputChanges = (event) => {
         const value = getInputValue(event.target);
         const inputType = event.target.getAttribute('data-input-type');
 
-        if (inputType === 'r1') {
+        if (inputType === 'r1' && isValidInput(value)) {
             processResistanceCalculations(value);
         }
     };
-
-    document
-        .querySelector('.rings-container')
-        .addEventListener('input', handleInputChanges);
 
     /**
      * Retrieves the numerical value from an input element
@@ -29,51 +34,71 @@
      * @returns {number} - The numerical value of the input
      */
     const getInputValue = (element) => {
-        return Number(element.value);
+        return Number(element.value) || 0;
     };
 
     /**
-     * Orchestrates the calculates and updating or resistance outputs
+     * Ensures that input value is a positive number
+     * @param {number} value - The resistance input value
+     * @returns {boolean} - True if valid
+     */
+    const isValidInput = (value) => {
+        return typeof value === 'number' && value > 0;
+    };
+
+    /**
+     * Initiates the resistance calculations and updates resistance outputs
      * @param {number} inputValue - The resistance input value (r1)
      */
     const processResistanceCalculations = (inputValue) => {
-        const outputs = calculateResistanceOutputs(inputValue);
-        updateResistanceOutputs(outputs);
+        calculateResistanceOutputs(inputValue);
+        updateResistanceOutputs();
     };
 
     /**
-     * Calculates the resistance output values based on r1 input
-     * @param {number} inputValue - The resistance input value (r1)
-     * @returns {number[]} - The calculated resistance outputs
+     * Computes the resistance output values based on r1 input value
+     * @param {number} inputValue - The resistance value of r1
      */
     const calculateResistanceOutputs = (inputValue) => {
-        let r2_1 = (inputValue * RESISTANCE_ONE_MULTIPLIER).toFixed(2);
-        let r2_2 = (inputValue * RESISTANCE_TWO_MULTIPLIER).toFixed(2);
-        let rSum_1 = ((inputValue + Number(r2_1)) / 4).toFixed(2);
-        let rSum_2 = ((inputValue + Number(r2_2)) / 4).toFixed(2);
+        outputs.r2_multiplier_1 = roundValue(
+            inputValue * DEFAULT_R2_MULTIPLIER_ONE
+        );
+        outputs.r2_multiplier_2 = roundValue(
+            inputValue * DEFAULT_R2_MULTIPLIER_TWO
+        );
 
-        return [r2_1, r2_2, rSum_1, rSum_2];
+        outputs.r_sum_1 = roundValue(
+            (inputValue + outputs.r2_multiplier_1) / DIVISION_FACTOR
+        );
+        outputs.r_sum_2 = roundValue(
+            (inputValue + outputs.r2_multiplier_2) / DIVISION_FACTOR
+        );
     };
 
     /**
-     * Updates the resistance output fields with the calculated values
-     * @param {number[]} outputs - The calculated resistance output values
+     * Rounds a number to two decimal places
+     * @param {number} number - The number to round
+     * @returns {number} - The rounded number
      */
-    const updateResistanceOutputs = (outputs) => {
-        const outputTypes = [
-            'r2_multiplier_1',
-            'r2_multiplier_2',
-            'r_sum_1',
-            'r_sum_2',
-        ];
+    const roundValue = (number) => {
+        return Number(number.toFixed(2));
+    };
 
-        outputTypes.forEach((type, index) => {
+    /**
+     * Updates the output fields with the calculated resistance values
+     */
+    const updateResistanceOutputs = () => {
+        Object.keys(outputs).forEach((type) => {
             const outputElement = document.querySelector(
                 `[data-output-type='${type}']`
             );
             if (outputElement) {
-                outputElement.value = outputs[index];
+                outputElement.value = outputs[type];
             }
         });
     };
+
+    document
+        .querySelector('.rings-container')
+        .addEventListener('input', handleInputChanges);
 })();
