6062a5f48566711dcd1a635e7cf92b989cf43f28
extf8 <extfield8@gmail.com>
2023-08-19 16:54:22 +0300

    Refactored code and Added comments for clarity

--- renderer.js
+++ renderer.js
@@ -1,20 +1,25 @@
+/**
+ * Module for Page Loading and Script Management
+ */
+
+// Constants
+const BUTTONS = document.querySelectorAll('button[data-load-page]');
+const CONTAINER = document.getElementById('pages_container');
+const DEFAULT_PAGE = './pages/rings.html';
+
+const SCRIPT_MAPPING = {
+    './pages/circuitsCount.html': './scriptsPages/circuitsCalculator.js',
+    './pages/externalImpedance.html': './scriptsPages/impedanceCalculator.js',
+    './pages/rings.html': './scriptsPages/resistanceCalculator.js',
+};
+
+// Initialization
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button[data-load-page]');
-    const container = document.getElementById('pages_container');
-    const defaultPage = './pages/rings.html';
-
-    // Script mapping
-    const scriptMapping = {
-        './pages/circuitsCount.html': './scriptsPages/circuitsCalculator.js',
-        './pages/externalImpedance.html': './scriptsPages/impedanceCalculator.js',
-        './pages/rings.html': './scriptsPages/resistanceCalculator.js',
-    };
-
-    const defaultButton = Array.from(buttons).find(
-        (button) => button.getAttribute('data-load-page') === defaultPage
+    const defaultButton = Array.from(BUTTONS).find(
+        (button) => button.getAttribute('data-load-page') === DEFAULT_PAGE
     );
 
-    buttons.forEach((button) => {
+    BUTTONS.forEach((button) => {
         button.addEventListener('click', handleButtonClick);
     });
 
@@ -22,49 +27,72 @@ document.addEventListener('DOMContentLoaded', () => {
         activateButton(defaultButton);
     }
 
-    loadPage(defaultPage, container);
-
-    function handleButtonClick(e) {
-        const pageToLoad = e.target.getAttribute('data-load-page');
-
-        deactivateButtons(buttons);
-        loadPage(pageToLoad, container);
-        activateButton(e.target);
-    }
-
-    function activateButton(button) {
-        button.classList.add('active');
-    }
-
-    function deactivateButtons(buttons) {
-        buttons.forEach((button) => button.classList.remove('active'));
-    }
-    // Load page
-    async function loadPage(url, container) {
-        try {
-            const response = await fetch(url);
-            const html = await response.text();
-            container.innerHTML = html;
-            loadScript(scriptMapping[url]);
-        } catch (error) {
-            console.error('Error loading page:', error);
-        }
-    }
-
-    // Load script
-    function loadScript(scriptUrl) {
-        // Remove old script
-        const oldScript = document.getElementById('calculations_script');
-        if (oldScript) {
-            oldScript.remove();
-        }
-
-        // Create and append the new script element
-        const script = document.createElement('script');
-        script.id = 'calculations_script';
-        script.src = scriptUrl;
-        script.defer = true;
-
-        document.head.appendChild(script);
-    }
+    loadPage(DEFAULT_PAGE, CONTAINER);
 });
+
+/**
+ * Event handler for button clicks.
+ * Determines the page to load and updates the UI accordingly
+ * @param {Event} event - The DOM event triggered by the button click
+ */
+const handleButtonClick = (event) => {
+    const pageToLoad = event.target.getAttribute('data-load-page');
+
+    deactivateButtons(BUTTONS);
+    loadPage(pageToLoad, CONTAINER);
+    activateButton(event.target);
+};
+
+/**
+ * Activates the specified button, adding an 'active' class
+ * @param {HTMLButtonElement} button - The button element to activate
+ */
+const activateButton = (button) => {
+    button.classList.add('active');
+};
+
+/**
+ * Deactivates all buttons in the provided list by removing the 'active' class
+ * @param {NodeList} BUTTONS - List of button elements to deactivate
+ */
+const deactivateButtons = (BUTTONS) => {
+    BUTTONS.forEach((button) => button.classList.remove('active'));
+};
+
+/**
+ * Loads the specified page in to container element
+ * Loads the associated script for that page
+ * @param {string} url - URL of the page to load
+ * @param {element} CONTAINER - The container element to load the page into
+ */
+async function loadPage(url, CONTAINER) {
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
+        CONTAINER.innerHTML = html;
+        loadScript(SCRIPT_MAPPING[url]);
+    } catch (error) {
+        console.error('Error loading page:', error);
+    }
+}
+
+/**
+ * Loads the script associated with the given URL.
+ * If an old script is present, removes it before loading the new one
+ * @param {string} scriptUrl - URL of the script to load
+ */
+const loadScript = (scriptUrl) => {
+    // Remove old script
+    const oldScript = document.getElementById('calculations_script');
+    if (oldScript) {
+        oldScript.remove();
+    }
+
+    // Create and append the new script element
+    const script = document.createElement('script');
+    script.id = 'calculations_script';
+    script.src = scriptUrl;
+    script.defer = true;
+
+    document.head.appendChild(script);
+};
