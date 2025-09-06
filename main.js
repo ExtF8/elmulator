020089c074d81964b5ada3ac0a1708bf57212382
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-09-06 18:12:54 +0300

    Extracted auto updater

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ const fs = require('fs');
 const { createMainWindow } = require('./window/createMainWindow');
 const { setupMainMenu } = require('./menu/menu');
 const { registerIcpHandlers } = require('./ipc');
-const dialogs  = require('./services/dialogs');
+const dialogs = require('./services/dialogs');
 const childRunner = require('./services/childRunner');
+const { startAutoUpdate } = require('./services/updater');
 
 // --- Single-instance lock (prevents second instance on Windows) ---
 const gotLock = app.requestSingleInstanceLock();
@@ -30,10 +31,6 @@ if (!gotLock) {
 
 // ENV for Hot Reloading in the development environment.
 const env = process.env.NODE_ENV;
-
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
 
 // If development environment
 if (env === 'development') {
@@ -65,6 +62,10 @@ app.whenReady().then(() => {
     // Register IPC handlers
     registerIcpHandlers({ dialogs, childRunner });
 
+    if (process.platform === 'win32' || 'darwin') {
+        startAutoUpdate(mainWindow);
+    }
+
     app.on('activate', () => {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
@@ -76,46 +77,5 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
-    }
-});
-
-// Additional main process code can be added here or in separate modules
-
-// Auto updater implementation
-app.on('ready', function () {
-    autoUpdater.checkForUpdatesAndNotify();
-});
-
-autoUpdater.on('checking-for-update', () => {
-    log.info('Checking for updates...');
-});
-
-autoUpdater.on('update-available', () => {
-    log.info('Update available. Downloading...');
-});
-
-autoUpdater.on('update-not-available', () => {
-    log.info('No update available...');
-});
-
-autoUpdater.on('error', err => {
-    log.info('Update error: ', err);
-});
-
-autoUpdater.on('download-progress', progress => {
-    log.info(`Download speed: ${progress.bytesPerSecond} B/s - ${Math.round(progress.percent)}%`);
-});
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, name) => {
-    const response = dialog.showMessageBoxSync(mainWindow, {
-        type: 'question',
-        buttons: ['Restart now', 'Later'],
-        title: `Update available: ${name}`,
-        message: 'New version downloaded. Would you like to install it now?',
-        detail: releaseNotes || 'No release notes provided.',
-    });
-
-    if (response === 0) {
-        autoUpdater.quitAndInstall(false, true);
     }
 });
